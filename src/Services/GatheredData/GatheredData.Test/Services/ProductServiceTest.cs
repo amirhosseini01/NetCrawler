f222a44107c35248c71f0508a6d5a6dfb84c6f23
amircsharp7 <amir.1derful@gmail.com>
2022-08-27 23:34:02 +0430

    Try to write test for mongo db

--- src/Services/GatheredData/GatheredData.Test/Services/ProductServiceTest.cs
+++ src/Services/GatheredData/GatheredData.Test/Services/ProductServiceTest.cs
@@ -1,10 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GatheredData.Api.Models;
+using GatheredData.Api.Services;
+using Microsoft.Extensions.Options;
 
 namespace GatheredData.Test.Services;
 public class ProductServiceTest
 {
+    private readonly IProductsService _productsService;
+
+    public ProductServiceTest()
+    {
+        ProductStoreDatabaseSettings productStoreDatabaseSettings = new()
+        {
+            ConnectionString = "mongodb://gathereddataapidb:27017",
+            DatabaseName = "NetCrawler",
+            ProductsCollectionName = "Products"
+        };
+
+        IOptions<ProductStoreDatabaseSettings> opt = Options.Create(productStoreDatabaseSettings);
+
+        _productsService = new ProductsService(opt);
+    }
+
+    [Fact]
+    public async Task GetAsync_Should_Return_ListOfProducts()
+    {
+        //arrange
+         ProductStoreDatabaseSettings productStoreDatabaseSettings = new()
+        {
+            ConnectionString = "mongodb://gathereddataapidb:27017",
+            DatabaseName = "NetCrawler",
+            ProductsCollectionName = "Products"
+        };
+
+        IOptions<ProductStoreDatabaseSettings> opt = Options.Create(productStoreDatabaseSettings);
+
+        var productsService = new ProductsService(opt);
+
+        //act
+        var result = await productsService.GetAsync();
+
+        //assert
+        Assert.NotNull(result);
+        Assert.IsType<List<Product>>(result);
+    }
 }
