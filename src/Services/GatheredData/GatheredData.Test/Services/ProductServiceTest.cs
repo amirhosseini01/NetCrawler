f0fcc99749ec2fdfcab8099a98b52a305a817769
amircsharp7 <amir.1derful@gmail.com>
2022-09-04 23:19:16 +0430

    Write test for some services methods

--- src/Services/GatheredData/GatheredData.Test/Services/ProductServiceTest.cs
+++ src/Services/GatheredData/GatheredData.Test/Services/ProductServiceTest.cs
@@ -29,4 +29,71 @@ public class ProductServiceTest
         Assert.NotNull(result);
         Assert.IsType<List<Product>>(result);
     }
+
+    [Theory]
+    [InlineData("61a6058e6c43f32854e51f51")]
+    public async Task GetAsync_Should_Return_Product(string id)
+    {
+        //act
+        var result = await _productsService.GetAsync(id);
+
+        //assert
+        Assert.NotNull(result);
+        Assert.IsType<Product>(result);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("61a6058e6c43f32854e51f00")]
+    public async Task GetAsync_Should_Return_Null(string id)
+    {
+        //act
+        var result = await _productsService.GetAsync(id);
+
+        //assert
+        Assert.Null(result);
+    }
+
+    [Theory]
+    [InlineData(" ")]
+    [InlineData("  ")]
+    [InlineData("1234")]
+    public async Task GetAsync_Should_Throw(string id)
+    {
+        //act & assert
+        await Assert.ThrowsAnyAsync<FormatException>(() => _productsService.GetAsync(id));
+    }
+
+    [Theory]
+    [InlineData("61a6058e6c43f32854e51f51")]
+    public async Task AnyAsync_Should_Return_True(string id)
+    {
+        //act
+        var result = await _productsService.AnyAsync(id);
+
+        //assert
+        Assert.True(result);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("61a6058e6c43f32854e51f00")]
+    public async Task AnyAsync_Should_Return_False(string id)
+    {
+        //act
+        var result = await _productsService.AnyAsync(id);
+
+        //assert
+        Assert.False(result);
+    }
+
+    [Theory]
+    [InlineData(" ")]
+    [InlineData("  ")]
+    [InlineData("1234")]
+    public async Task AnyAsync_Should_Throw(string id)
+    {
+        //act & assert
+        await Assert.ThrowsAnyAsync<FormatException>(() =>  _productsService.AnyAsync(id));
+    }
 }
